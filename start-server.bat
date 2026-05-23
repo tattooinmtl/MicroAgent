a1906857ce82c23db762f4c29f58f413c21a35b9
TattooPeel <94817530+tattooinmtl@users.noreply.github.com>
2026-05-23 12:55:11 -0400

    Update start-server.bat

--- start-server.bat
+++ start-server.bat
@@ -1,5 +1,5 @@
 @echo off
 echo.
 echo  Starting AI server - Gemma 4 E2B
-start cmd /k "C:\AgentQc\OpenClaude-Portable-main\newllamacpp\llama-server.exe -m C:\canpro\llama-interface\models\gemma-4-E2B-it-Q4_K_M.gguf --port 8080 -c 6144"
+start cmd /k "C:\llama\llama-server.exe -m C:\llama\models\gemma-4-E2B-it-Q4_K_M.gguf --port 8080 -c 6144"
 pause
